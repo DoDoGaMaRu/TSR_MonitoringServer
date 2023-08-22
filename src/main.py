d9b45d2ecb16efc2e88602c0e94994ed6a0b3c8a
DoDoGaMaRu <101071839+DoDoGaMaRu@users.noreply.github.com>
2023-08-22 13:19:54 +0900

    [Release : 0.0.0_base] - 기본 기능 구성 완료 - 기반 코드 최초 배포
    Versioning 규칙은 Semantic Versioning 을 따르는 것을 원칙으로 함.
    
    {major}.{minor}.{patch}
    Major: 이전 버전과 호환이 안 되는 변경이 있다면 숫자를 올립니다
    Minor: 이전 버전과 호환되는 기능 추가가 있다면 숫자를 올립니다.
    Patch: 이전 버전의 버그를 수정했다면 숫자를 올립니다.

--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
+import multiprocessing
+
 from monitoring_app import MonitoringApp
 
 
 # TODO 서버는 프롬프트 화면이 나오지 않게 하고, 중복 실행 방지 로직 추가
 if __name__ == '__main__':
+    multiprocessing.freeze_support()
+
     server = MonitoringApp()
     server.run()
